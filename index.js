49fa7802693567bd740fb6c5a2d1818fab039e9d
Carlos Borrega <c.borrega@proton.me>
2022-11-24 12:40:31 +0000

    contact page created

---
+++ index.js
@@ -0,0 +1,10 @@
+function copyToClipboard() {
+
+    var copyText = document.getElementById("email");
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyText.value);
+  
+  }
