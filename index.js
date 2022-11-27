2d8e5bcc1057c36a6b20730f951483d96cc06129
Carlos Borrega <c.borrega@proton.me>
2022-11-27 10:17:24 +0000

    Profile photo added

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function copyToClipboard() {
     copyText.select();
     copyText.setSelectionRange(0, 99999);
   
-    navigator.clipboard.writeText(copyText.value);
+    navigator.clipboard.writeText(copyText.value);    
     
   }
