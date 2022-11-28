06d9bd8f4cc6cc0d91c614670cfbf9e2cdd05ec4
Carlos Borrega <c.borrega@proton.me>
2022-11-28 13:34:49 +0000

    mobile and tablet view

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+// Change text
+
+function changeText() {
+  document.getElementById("hello").innerHTML = "About"
+  document.getElementById("iam").innerHTML = "Projects"
+  document.getElementById("name").innerHTML = "Contact"
+
+}
+
+
 // "Copy to clipboard" text
 
 function copyToClipboard() {
 
-    var copyText = document.getElementById("email");
-  
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-  
-    navigator.clipboard.writeText(copyText.value);    
-    
-  }
+  var copyText = document.getElementById("email");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+
